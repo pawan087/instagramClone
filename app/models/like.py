7e678ff34f432c2eff9743692d41f367bdb359d3
Ismail <inmanjlai@hotmail.com>
2021-10-08 13:56:20 -0400

    likes table complete

--- app/models/like.py
+++ app/models/like.py
@@ -1,4 +1,14 @@
 from .db import db
 
 class Like(db.Model):
+    
     __tablename__ = "likes"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    image_id = db.Column(db.Integer, db.ForeignKey("images.id"), nullable=False)
+
+    user = db.relationship("User", back_populates="likes")
+    image = db.relationship("Image", back_populates="likes")
+
+
