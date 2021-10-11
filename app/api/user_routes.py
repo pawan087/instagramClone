811dc1f77dd04afffb500a813ee03fd46ac429be
Pawan Chahal <chahal.pawanpreet@gmail.com>
2021-10-11 10:40:17 -0700

    adding post follower post route

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_login import login_required
-from app.models import User
+from app.models import User, db
+from app.forms.follow_form import DeleteFollow, NewFollow
 
 user_routes = Blueprint('users', __name__)
 
@@ -12,8 +13,17 @@ def users():
     return {'users': [user.to_dict() for user in users]}
 
 
-@user_routes.route('/<int:id>')
-@login_required
-def user(id):
-    user = User.query.get(id)
-    return user.to_dict()
+@user_routes.route('/following/', methods=["POST"])
+def add_follow():
+    form = NewFollow()
+    data = form.data
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        user_to_follow = User.query.get(data['user_to_follow_id'])
+        current_user = User.query.get(data['current_user_id'])
+
+        db.session.commit()
+        # users = User.query.all()
+        # return {'users': [user.to_dict() for user in users]}
+    else:
+        return "Bad Data"
