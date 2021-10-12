630e67daa733b50725c188ff9c42efb85d6809e0
Ismail <inmanjlai@hotmail.com>
2021-10-12 13:07:08 -0400

    finished backend implementation of events

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -3,6 +3,7 @@ from .users import seed_users, undo_users
 from .images import seed_images, undo_images
 from .likes import seed_likes, undo_likes
 from .comments import seed_comments, undo_comments
+from .events import seed_events, undo_events
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -16,6 +17,7 @@ def seed():
     seed_images()
     seed_likes()
     seed_comments()
+    seed_events()
     # Add other seed functions here
 
 
@@ -26,4 +28,5 @@ def undo():
     undo_images()
     undo_comments()
     undo_likes()
+    undo_events()
     # Add other undo functions here
