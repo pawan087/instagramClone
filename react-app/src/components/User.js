8a292837da2d905845b1817781031cd049b124b4
Michael E <mceng.design@gmail.com>
2021-10-11 23:39:16 -0700

    basic structure and structure styling for profile_top area

--- react-app/src/components/User.js
+++ react-app/src/components/User.js
@@ -4,7 +4,7 @@ import { useParams } from 'react-router-dom';
 import { setAllImages } from '../store/image';
 import ImageComponent from './Images/ImageComponent';
 import { addFollow, deleteFollow, setAllUsers } from '../store/session';
-
+import './user.css'
 function User() {
   const { userId }  = useParams();
   const dispatch = useDispatch()
@@ -40,17 +40,45 @@ function User() {
 
   return (
     <>
-    <div>
-        <form onSubmit={addOrRemoveFollow}>
-            {currentPagesUser?.followers?.includes(curUser.id) ? <button>Unfollow</button> : <button>follow</button>}
-        </form>
-    </div>
-    <div className="imageContainer">
-            {usersImages?.map((image) => (
-                <ImageComponent image={image} key={image.id} />
-            ))}
-        </div>
-        </>
+      <div className="profileTop">
+          <div className="profileAvatarBox">
+              <div className="profileAvatarContainer">
+                  {/* <img src={image?.user?.avatar} alt="" /> */}
+              </div>
+          </div>
+          <div className="profileBox">
+              <div className="profileNameAndButtons">
+                  <div className="profileName">
+                      USERNAME GOES HERE
+                  </div>
+                  <div className="profileButtons">
+                      <form onSubmit={addOrRemoveFollow}>
+                          {currentPagesUser?.followers?.includes(curUser.id) ? <button>Unfollow</button> : <button>follow</button>}
+                      </form>
+                  </div>
+              </div>
+              <div className="profileCounts">
+                  <div className="profilePosts">#### Posts</div>
+                  <div className="profileFollowers">#### Followers</div>
+                  <div className="profileFollowing">#### Following</div>
+              </div>
+              <div className="profileName">
+                  NAME GOES HERE (optional)
+              </div>
+              <div className="profileBio">
+                  BIO GOES HERE
+              </div>
+              <div>
+                FOLLOWED BY GOES HERE
+              </div>
+          </div>
+      </div>
+      <div className="imageContainer">
+          {usersImages?.map((image) => (
+              <ImageComponent image={image} key={image.id} />
+          ))}
+      </div>
+    </>
   );
 }
 export default User;
