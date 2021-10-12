7e72fac695b6f33dca54fc80e3d3a3cc9e5077a4
Ismail <inmanjlai@hotmail.com>
2021-10-12 15:16:02 -0400

    better

--- react-app/src/components/Notifications/index.jsx
+++ react-app/src/components/Notifications/index.jsx
@@ -1,15 +1,14 @@
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 
 const Notifications = () => {
 
-    const dispatch = useDispatch()
     const user = useSelector((state) => state.session.user)
 
     console.log(user.incoming_events.incoming)
 
     return (
         <div>
-            <h3>This is an event</h3>
+            {/* <h3>This is an event</h3> */}
         </div>
     )
 }
