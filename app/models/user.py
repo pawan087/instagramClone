463d301f73beafc2144accc6b5f85e6f662ed248
Ismail <inmanjlai@hotmail.com>
2021-10-13 13:44:52 -0400

    finished user overhaul

--- app/models/user.py
+++ app/models/user.py
@@ -13,8 +13,11 @@ class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    avatar = db.Column(db.String(2000), nullable=True,
-                       default='https://i.imgur.com/RBkqFEg.jpg')
+    avatar = db.Column(db.String(2000), nullable=True, default='https://i.imgur.com/RBkqFEg.jpg')
+    fname = db.Column(db.String(100), nullable=False)
+    lname = db.Column(db.String(100), nullable=False)
+    bio = db.Column(db.String(500), nullable=True)
+    pronouns = db.Column(db.String(100), nullable=True)
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
@@ -49,6 +52,10 @@ class User(db.Model, UserMixin):
         return {
             'id': self.id,
             'username': self.username,
+            'fname': self.fname,
+            'lname': self.lname,
+            'bio': self.bio,
+            'pronouns': self.pronouns,
             'email': self.email,
             'followers': self.followers,
             'following': self.following,
