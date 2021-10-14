bce6f303041eb9f7d3a2e86dfe9647662fd0f149
Pawan Chahal <chahal.pawanpreet@gmail.com>
2021-10-14 14:52:31 -0700

    user able to update profile without a new avatar, uses existing

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,4 +1,3 @@
-
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
@@ -8,10 +7,8 @@ import post from "../image_assets/post.svg"
 import notifications from "../image_assets/notifications.svg"
 import { deleteOneEvent, startPolling } from '../store/event';
 
-
-
 const NavBar = () => {
-  
+
   useEffect(() => {
     let notificationButton = document.querySelector(".dropdown_menu")
     let content = document.querySelector(".dropdown_content")
@@ -20,22 +17,19 @@ const NavBar = () => {
     })
   }, [])
 
-
-  useEffect(() => {
-    dispatch(startPolling(user.id))
-  },[])
- 
-  
+  // useEffect(() => {
+  //   dispatch(startPolling(user.id))
+  // },[])
 
   const dispatch = useDispatch()
   const user = useSelector((state) => state.session.user)
   const allUsers = useSelector((state) => state.session.allUsers)
   const events = useSelector((state) => state.events)
-  
+
   const findUser = (userId) => {
     return allUsers?.filter((user) => user.id === userId)[0]
   }
-  
+
   return (
     <nav>
       <h1>
@@ -51,8 +45,8 @@ const NavBar = () => {
           <div className='dropdown_menu'>
             <img src={notifications} alt="home" className="home_button button" draggable="false" />
             <div className="dropdown_content">
-              {events.length > 0 ? 
-                events.map((event) => 
+              {events.length > 0 ?
+                events.map((event) =>
                   (
                     <div key={event.id}>
                       <div className="userInfo">
@@ -66,7 +60,7 @@ const NavBar = () => {
             </div>
           </div>
         )}
-        
+
         {user?.id && <NavLink to='/images/new' exact={true} activeClassName='active' draggable="false"><img src={post} alt="postPicture" className='home_button button' draggable="false" /></NavLink>}
         {user?.id && <NavLink to={`/users/${user.id}`} exact={true} activeClassName='active' draggable="false">
           <div className="avatarContainer button">
