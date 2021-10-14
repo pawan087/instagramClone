3332d02d88d488d3a0d90d631dabc2ce323294bb
baselHassan <baselhassan8@gmail.com>
2021-10-14 05:41:34 -0500

    placed fields correctly, set up image container

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { login } from '../../store/session';
+import './loginForm.css';
+import frame from '../../image_assets/frame.png'
+
 
 const LoginForm = () => {
   const [errors, setErrors] = useState([]);
@@ -31,34 +34,47 @@ const LoginForm = () => {
   }
 
   return (
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
+    <div className="pageContainer">
+      <div className="main">
+        <img src={frame} alt='rotating phone images' className='frame' />
+        <form onSubmit={onLogin} className='loginContainer'>
+          <div>
+            {errors.map((error, ind) => (
+              <div key={ind}>{error}</div>
+            ))}
+          </div>
+          <div className="loginTitle">Kilogram</div>
+          <div className="loginFormContainer">
+            <div>
+              <input
+                className='loginField'
+                name='email'
+                type='text'
+                placeholder='Email'
+                value={email}
+                onChange={updateEmail}
+              />
+            </div>
+            <div>
+              <input
+                className='loginField'
+                name='password'
+                type='password'
+                placeholder='Password'
+                value={password}
+                onChange={updatePassword}
+              />
+            </div>
+            <div className="loginBtn">
+              <button className='btn' type='submit'>Login</button>
+            </div>
+            <div className="orContainer">
+              <span className="orText">OR</span>
+            </div>
+          </div>
+        </form>
       </div>
-      <div>
-        <label htmlFor='email'>Email</label>
-        <input
-          name='email'
-          type='text'
-          placeholder='Email'
-          value={email}
-          onChange={updateEmail}
-        />
-      </div>
-      <div>
-        <label htmlFor='password'>Password</label>
-        <input
-          name='password'
-          type='password'
-          placeholder='Password'
-          value={password}
-          onChange={updatePassword}
-        />
-        <button type='submit'>Login</button>
-      </div>
-    </form>
+    </div>
   );
 };
 
