5033c07356c112a0a2699cd46482fa630ad906d3
Pawan Chahal <chahal.pawanpreet@gmail.com>
2021-10-13 21:10:03 -0700

    finished styling of sign up form

--- react-app/src/components/auth/testSignUpForm.js
+++ react-app/src/components/auth/testSignUpForm.js
@@ -7,9 +7,7 @@ export default function testSignUpForm() {
       <div className="topInnerContainer">
         <div className="title logo">Kilogram</div>
 
-        <div className="subtitle">
-          Sign up to see photos from your friends.
-        </div>
+        <div className="subtitle">Sign up to see photos from your friends.</div>
 
         <div className="btnContainer">
           <button className="demoBtn">Log in as Demo User</button>
@@ -64,6 +62,33 @@ export default function testSignUpForm() {
 
               <option value="Other">Other</option>
             </select>
+          </div>
+
+          <div className="uploadDiv fileinputs">
+            <input
+              className="inputContainer file"
+              type="file"
+              accept="image/*"
+            />
+            <div class="inputContainer fakefile">
+              <label className="uploadLabel">Upload Profile Picture</label>
+              <div className='uploadPic'>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"
+                  />
+                </svg>
+              </div>
+            </div>
           </div>
 
           <div>
