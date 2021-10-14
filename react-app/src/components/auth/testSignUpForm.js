c1240ad16a27c28e6d5b32bcedcfa7a1f8644c3a
Pawan Chahal <chahal.pawanpreet@gmail.com>
2021-10-13 21:44:41 -0700

    connected inputs to new form, controlled inputs, debugging submission to backend

--- react-app/src/components/auth/testSignUpForm.js
+++ react-app/src/components/auth/testSignUpForm.js
@@ -1,9 +1,102 @@
-import React from "react";
 import "./signUpForm.css";
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Redirect } from "react-router-dom";
+import { signUp } from "../../store/session";
 
-export default function testSignUpForm() {
+const TestSignUpForm = () => {
+  const [errors, setErrors] = useState([]);
+  const [username, setUsername] = useState("testUsername");
+  const [email, setEmail] = useState("test@email.com");
+  const [password, setPassword] = useState("password");
+  const [repeatPassword, setRepeatPassword] = useState("password");
+  const [image, setImage] = useState(null);
+  const [imageLoading, setImageLoading] = useState(false);
+  const [fname, setFname] = useState("testFname");
+  const [lname, setLname] = useState("testLname");
+  const [bio, setBio] = useState("testBio");
+  const [pronouns, setPronouns] = useState("He/Him");
+
+  const user = useSelector((state) => state.session.user);
+  const dispatch = useDispatch();
+
+  const onSignUp = async (e) => {
+    e.preventDefault();
+
+    if (password === repeatPassword) {
+      const formData = new FormData();
+
+      formData.append("username", username);
+      formData.append("email", email);
+      formData.append("password", password);
+      formData.append("avatar", image);
+      formData.append("fname", fname);
+      formData.append("lname", lname);
+      formData.append("bio", bio);
+      formData.append("pronouns", pronouns);
+
+      // setImageLoading(true);
+
+      const data = await dispatch(signUp(formData));
+
+      // setImageLoading(false);
+
+      if (data) {
+        setErrors(data);
+      }
+    }
+  };
+
+  const updateUsername = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const updateEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const updatePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const updateRepeatPassword = (e) => {
+    setRepeatPassword(e.target.value);
+  };
+
+  const updateFname = (e) => {
+    setFname(e.target.value);
+  };
+
+  const updateLname = (e) => {
+    setLname(e.target.value);
+  };
+
+  const updatePronouns = (e) => {
+    setPronouns(e.target.value);
+  };
+
+  const updateBio = (e) => {
+    setBio(e.target.value);
+  };
+
+  const updateImage = (e) => {
+    const file = e.target.files[0];
+
+    setImage(file);
+  };
+
+  if (user) {
+    return <Redirect to="/" />;
+  }
+
   return (
-    <div className="outerContainer">
+    <form className="outerContainer" onSubmit={onSignUp}>
+      <div>
+        {errors.map((error, ind) => (
+          <div key={ind}>{error}</div>
+        ))}
+      </div>
+
       <div className="topInnerContainer">
         <div className="title logo">Kilogram</div>
 
@@ -22,7 +115,21 @@ export default function testSignUpForm() {
             <input
               className="inputContainer inputText"
               type="text"
+              placeholder="Username"
+              name="username"
+              onChange={updateUsername}
+              value={username}
+            ></input>
+          </div>
+
+          <div>
+            <input
+              className="inputContainer inputText"
+              type="text"
               placeholder="Email"
+              name="email"
+              onChange={updateEmail}
+              value={email}
             ></input>
           </div>
 
@@ -30,6 +137,9 @@ export default function testSignUpForm() {
             <input
               className="inputContainer"
               type="text"
+              placeholder="First Name"
+              onChange={updateFname}
+              value={fname}
               placeholder="First Name"
             ></input>
           </div>
@@ -39,19 +149,19 @@ export default function testSignUpForm() {
               className="inputContainer"
               type="text"
               placeholder="Last Name"
+              onChange={updateLname}
+              value={lname}
+              placeholder="Last name"
             ></input>
           </div>
 
           <div>
-            <input
+            <select
               className="inputContainer"
               type="text"
-              placeholder="Bio"
-            ></input>
-          </div>
-
-          <div>
-            <select className="inputContainer" type="text">
+              onChange={updatePronouns}
+              value={pronouns}
+            >
               <option value={null}>Pronouns</option>
 
               <option value="He/Him">He/Him</option>
@@ -64,15 +174,26 @@ export default function testSignUpForm() {
             </select>
           </div>
 
+          <div>
+            <input
+              className="inputContainer"
+              type="text"
+              placeholder="Bio"
+              onChange={updateBio}
+              value={bio}
+            ></input>
+          </div>
+
           <div className="uploadDiv fileinputs">
             <input
               className="inputContainer file"
               type="file"
               accept="image/*"
+              onChange={updateImage}
             />
             <div class="inputContainer fakefile">
               <label className="uploadLabel">Upload Profile Picture</label>
-              <div className='uploadPic'>
+              <div className="uploadPic">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="h-6 w-6"
@@ -94,22 +215,31 @@ export default function testSignUpForm() {
           <div>
             <input
               className="inputContainer"
-              type="text"
+              type="password"
               placeholder="Password"
+              name="password"
+              onChange={updatePassword}
+              value={password}
             ></input>
           </div>
 
           <div>
             <input
               className="inputContainer"
-              type="text"
+              type="password"
               placeholder="Repeat Password"
+              name="repeat_password"
+              onChange={updateRepeatPassword}
+              value={repeatPassword}
+              required={true}
             ></input>
           </div>
         </div>
 
         <div className="btnContainer2">
-          <button className="signUpBtn">Sign Up</button>
+          <button type="submit" className="signUpBtn">
+            Sign Up
+          </button>
         </div>
 
         <div className="policyContainer">
@@ -125,6 +255,8 @@ export default function testSignUpForm() {
           Log in
         </a>
       </div>
-    </div>
+    </form>
   );
-}
+};
+
+export default TestSignUpForm;
