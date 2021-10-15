897fd84851680045a490cb334a54a0fa7529f90a
Pawan Chahal <chahal.pawanpreet@gmail.com>
2021-10-15 10:25:24 -0700

    fixed footer links to navigate to new tab

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -5,16 +5,15 @@ import githubImg from '../../image_assets/GitHub-Mark-32px.png'
 
 
 const Footer = () => {
-
   return (
     <footer className="footer">
       <div className="footerContent">
         <div className="footerTop">
           <div className="gitIconContainer"><img src={githubImg} alt="Github Logo" /></div>
-          <div className="gitLink"><a href="https://github.com/pawan087">Pawan Chahal</a></div>
-          <div className="gitLink"><a href="https://github.com/mce-design">Michael Eng</a></div>
-          <div className="gitLink"><a href="https://github.com/BaselHassan8">Basel Hassan</a></div>
-          <div className="gitLink"><a href="https://github.com/ocahsa">Ismail Manjlai</a></div>
+          <div className="gitLink"><a target="_blank" href="https://github.com/pawan087">Pawan Chahal</a></div>
+          <div className="gitLink"><a target="_blank" href="https://github.com/mce-design">Michael Eng</a></div>
+          <div className="gitLink"><a target="_blank" href="https://github.com/BaselHassan8">Basel Hassan</a></div>
+          <div className="gitLink"><a target="_blank" href="https://github.com/ocahsa">Ismail Manjlai</a></div>
         </div>
         <div className="footerBottom">
           <div className="footerCopyright">
