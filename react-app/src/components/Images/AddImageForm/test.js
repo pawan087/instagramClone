9d04634cddc259a266b6ba24e10b46f220eec902
Pawan Chahal <chahal.pawanpreet@gmail.com>
2021-10-14 21:33:38 -0700

    fixed minor bugs with sign in button and photo upload button

--- react-app/src/components/Images/AddImageForm/test.js
+++ react-app/src/components/Images/AddImageForm/test.js
@@ -68,153 +68,111 @@ const TestAddImageForm = () => {
     setImage(file);
   };
 
-  console.log(image);
+  const goBack = (e) => {
+    e.preventDefault();
+
+    history.push("/");
+  };
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Image Title</label>
-
-          <input
-            type="text"
-            placeholder="Image Title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
+    <div className="outerContainer1">
+      <div className="topContainer">
+        <div onClick={goBack} className="backBtn">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M10 19l-7-7m0 0l7-7m-7 7h18"
+            />
+          </svg>
         </div>
-
-        <div>
-          <label>Caption</label>
-
-          <input
-            type="text"
-            placeholder="Image Caption"
-            value={caption}
-            onChange={(e) => setCaption(e.target.value)}
-          />
-        </div>
-
-        <div className="input-file-container">
-          <input
-            type="file"
-            accept="image/*"
-            onChange={updateImage}
-            className="inpuut-file"
-            id="my-file"
-          />
-
-          <label tabindex="0" for="my-file" class="input-file-trigger">
-            Select a file...
-          </label>
-
-          {imageLoading && <p>Loading...</p>}
-        </div>
-
-        <div>
-          <label>Hashtags</label>
-
-          <input
-            type="text"
-            placeholder="Hashtags"
-            value={hashtags}
-            onChange={(e) => setHashtags(e.target.value)}
-          />
-          <p>Separate tags by spaces</p>
-        </div>
-
-        <button>Submit</button>
-      </form>
-      <div className="picContainer">
-        <img className="pic" src={preview}></img>
+        <p className="header">Compose</p>
       </div>
 
-      <div className="outerContainer">
-        <div className="topContainer">
-          <div className="backBtn">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M10 19l-7-7m0 0l7-7m-7 7h18"
+      <div className="bottomContainer">
+        <div className="leftContainer">
+          {!preview && (
+            <div className="input-file-container">
+              <input
+                type="file"
+                accept="image/*"
+                onChange={updateImage}
+                className="inpuut-file"
+                id="my-file"
               />
-            </svg>
-          </div>
-          <p className="title">Compose</p>
+
+              <label tabindex="0" for="my-file" class="input-file-trigger">
+                Select a file...
+              </label>
+            </div>
+          )}
+
+          {preview && <img class="picToUpload pic2" src={preview} />}
         </div>
 
-        <div className="bottomContainer">
-          <div className="leftContainer">
-            {!preview && (
-              <div className="input-file-container">
-                <input
-                  type="file"
-                  accept="image/*"
-                  onChange={updateImage}
-                  className="inpuut-file"
-                  id="my-file"
-                />
+        <div className="rightContainer">
+          <div className="rightOne">
+            <img src={user?.avatar} className="avatar"></img>
+            <p className="username">{user?.username}</p>
+          </div>
 
-                <label tabindex="0" for="my-file" class="input-file-trigger">
-                  Select a file...
-                </label>
-              </div>
+          <div className="rightTwo">
+            <textarea
+              value={caption}
+              onChange={(e) => setCaption(e.target.value)}
+              className="caption2"
+              placeholder="Write a caption..."
+            />
+          </div>
+
+          <div className="rightThree">
+            <input
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder="Add Title..."
+              type="text"
+              className="title"
+            />
+          </div>
+
+          <div className="rightFour">
+            <input
+              placeholder="Add Hashtags..."
+              type="text"
+              value={hashtags}
+              onChange={(e) => setHashtags(e.target.value)}
+              className="hashtags"
+            />
+          </div>
+
+          <div className="emptyDiv divisor" />
+
+          <div className="btnContainer">
+            {image && (
+              <button onClick={handleSubmit} className="btn1">
+                Share
+              </button>
             )}
-
-            {preview && <img class="picToUpload pic2" src={preview} />}
-          </div>
-
-          <div className="rightContainer">
-            <div className="rightOne">
-              <img src={user?.avatar} className="avatar"></img>
-              <p className="username">{user?.username}</p>
-            </div>
-
-            <div className="rightTwo">
-              <textarea
-                value={caption}
-                onChange={(e) => setCaption(e.target.value)}
-                className="caption2"
-                placeholder="Write a caption..."
-              />
-            </div>
-
-            <div className="rightThree">
-              <input
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-                placeholder="Add Title..."
-                type="text"
-                className="title"
-              />
-            </div>
-
-            <div className="rightFour">
-              <input
-                placeholder="Add Hashtags..."
-                type="text"
-                value={hashtags}
-                onChange={(e) => setHashtags(e.target.value)}
-                className="hashtags"
-              />
-            </div>
-
-            <div className="emptyDiv divisor" />
-
-            <div className="btnContainer">
-              <button onClick={handleSubmit} className="btn">Share</button>
-            </div>
+            {!image && <button className="btn2">Share</button>}
           </div>
         </div>
       </div>
-    </>
+
+      {imageLoading && (
+        <div className="loadingModal">
+          <div className="logo innerModal">
+            Uploading <span class="dot-elastic2"></span>
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
 
