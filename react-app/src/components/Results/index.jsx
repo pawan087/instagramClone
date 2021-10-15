91970292bfcd2c13c6554deb1261caf7c2d0c209
Pawan Chahal <chahal.pawanpreet@gmail.com>
2021-10-14 22:14:48 -0700

    fixed bug with ordering of search results (newest to oldest)

--- react-app/src/components/Results/index.jsx
+++ react-app/src/components/Results/index.jsx
@@ -24,7 +24,7 @@ const Results = () => {
         <div className="imageContainer">
           {tagImages?.map((image) => (
             <ImageComponent image={image} />
-          ))}
+          )).reverse()}
         </div>
       ) : (
         <div className="noResultsContainer">
